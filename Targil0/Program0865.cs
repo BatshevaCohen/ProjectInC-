3e8d76250e40819449dd7a843241e7fc20230e53
shirelk <shkadosh@g.jct.ac.il>
2021-10-10 12:22:47 +0300

    Initial code + Welcome functions

---
+++ Targil0/Program0865.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome0865();
+            Welcome5626();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome5626();
+        private static void Welcome0865()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name;
+            name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my firt console application", name);
+        }
+    }
+}

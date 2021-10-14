9a7268af3c5b55ec3baa27d5e1959f8c4b784884
shirelk <shkadosh@g.jct.ac.il>
2021-10-14 12:40:10 +0300

    EX1- without classes

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stam();
+            Console.ReadKey();
+        }
+
+        private static void Stam()
+        {
+            IDAL.DO.Client client = new IDAL.DO.Client
+            {
+                ID = 123,
+                Name = "kuku",
+                Telephone = "0556789147",
+                Latitude = -36.123456,
+                Longitude = 29.654321
+            };
+            Console.WriteLine(client);
+        }
+    }
+}

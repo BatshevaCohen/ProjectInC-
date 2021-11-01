4632af867fc6d98452ed0d4df9ec1c3497ea3d78
shirelk <shkadosh@g.jct.ac.il>
2021-11-01 14:50:26 +0200

    tamshichu levad

--- DAL/Parcel.cs
+++ DAL/Parcel.cs
@@ -6,7 +6,6 @@ namespace IDAL
     {
         public struct Parcel
         {
-
             public int Id { get; set; }
             public int SenderId { get; set; }
             public int TargetId { get; set; }

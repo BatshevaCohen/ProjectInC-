0e8dfa14244c1ee32afec55fdb3911ad4cdbf090
shirelk <shkadosh@g.jct.ac.il>
2021-11-23 12:59:20 +0200

    fixes

--- BL/BLObject.cs
+++ BL/BLObject.cs
@@ -23,73 +23,15 @@ namespace IBL.BO
             drones = new List<DroneToList>();
            // dal = new DalObject.DalObject();
         }
-        //ADD:
-        public void AddStation(Station station)
-        {
-            IDAL.DO.Station s = new IDAL.DO.Station();
-            s.Name = station.Name;
-            s.Id = station.Id;
-            s.Longitude = station.Location.Longitude;
-            s.Latitude = station.Location.Latitude;
-            dalo.AddStation(s);//send the new station to DAL 
-            throw new NotImplementedException();
-        }
-        public int AddDrone(Drone drone, int stationId)
-        {
-            IDAL.DO.Drone d = new IDAL.DO.Drone();
-            d.Id = drone.Id;
-            d.Model = drone.Model;
-            d.MaxWeight = (WeightCategories)drone.Weight;
-            dalo.AddDrone(d);
 
-
-            dalo.UpdateDroneToStation(stationId, d);
-           
-            
-           
-            throw new NotImplementedException();
-        }
-        public void AddCustomer(Customer customer)
-        {
-            IDAL.DO.Customer c = new IDAL.DO.Customer();
-            c.Id = customer.Id;
-            c.Name = customer.Name;
-            c.Phone = customer.Phone;
-            dalo.AddCustomer(c);
-            
-            throw new NotImplementedException();
-        }
-        public void AddParcel(Parcel parcel)
-        {
-            parcel.Id = ++(DalObject.DO.DataSource.OrdinalNumber); //static serial number for parcel id
-            parcel.ParcelCreationTime = DateTime.Now;
-            parcel.AssignmentToParcelTime = ZeroTime;
-            parcel.CollectionTime = ZeroTime;
-            parcel.SupplyTime = ZeroTime;
-            parcel.DroneInParcel = null;
-            IDAL.DO.Parcel p = new IDAL.DO.Parcel();
-
-            p.Id = parcel.Id;
-            p.SenderId = parcel.Sender.Id;
-            p.ReceiverId = parcel.Resiver.Id;
-            p.Weight = (WeightCategories)parcel.Weight;
-            p.Priority = (Priorities)parcel.Priority;
-            dalo.AddParcel(p);
-            
-        }
+        
+        
         //UPDATE:
         public void UpdateDroneName(int id, string model)
         {
             dalo.UpdateNameOfDrone(id, model);
         }
-        public void UpdateStetion(int id, string name, int charging_spots)
-        {
-            dalo.UpdateStetion(id, name, charging_spots);
-        }
-        public void UpdateCustomer(int id, string name, string phone)
-        {
-            dalo.UpdateCustumer(id, name, phone);
-        }
+        
         //sending drone to charge
         public void UpdateChargeDrone(int droneId)
         {

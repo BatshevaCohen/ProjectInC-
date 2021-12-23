b44bf37ca751a2874b11c5662bbcf10a1aea8062
shirelk <shkadosh@g.jct.ac.il>
2021-12-23 18:17:08 +0200

    using in the PL

--- PL/DroneInActionView.xaml.cs
+++ PL/DroneInActionView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using BO;
+using BlApi;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- PL/DroneListWindow.xaml.cs
+++ PL/DroneListWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using DalApi;
 using BO;
-
+using BlApi;
 
 namespace PL
 {

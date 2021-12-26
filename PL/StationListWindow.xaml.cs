b9d7b987cc3c4e5a342b45b5886e2cd749f33f32
Batsheva <bat211735626@gmail.com>
2021-12-26 04:12:46 +0200

    stationWindow not fixed

---
+++ PL/StationListWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using BO;
+
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for StationListWindow.xaml
+    /// </summary>
+    public partial class StationListWindow : Window
+    {
+        BO.IBL bL;
+        public StationListWindow(BO.IBL bl)
+        {
+            this.bL = bl;
+            InitializeComponent();
+            
+        }
+        
+
+        private void ClearStatus_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void Availble_charging_spote_station_Click(object sender, RoutedEventArgs e)
+        {
+            StationsListView.ItemsSource = bL.ShowStationList().Where(x => x.AvailableChargingSpots > 0);
+        }
+
+        private void Station_List_Click(object sender, RoutedEventArgs e)
+        {
+            StationsListView.ItemsSource = bL.ShowStationList().ToList();
+        }
+        private void StationListView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            BO.Station? station = StationsListView.SelectedItem as BO.Station;
+            if (station != null)
+            {
+                new StationInActionView(station, bL,this).Show();
+            }
+        }
+    }
+}
